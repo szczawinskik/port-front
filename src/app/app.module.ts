9a1df4137af31a7cd117ad2dca576c5046785761
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-04-26 11:19:13 +0200

    user switching

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { CommonsModule } from 'src/commons/commons.module';
+import { AuthService } from 'src/commons/services/auth.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,10 @@ import { FooterComponent } from './layout/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonsModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/commons/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  get isAdmin() {
+    return this.authService.isAdmin;
+  }
+
+  setAdmin() {
+    this.authService.setAdmin();
+  }
+  setUser() {
+    this.authService.setUser();
+  }
 }
