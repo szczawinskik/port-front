f21a77404a348856c1485956440a2c71e74145b8
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-05-06 20:17:36 +0200

    header component tests

--- src/app/layout/header/header.component.spec.ts
+++ src/app/layout/header/header.component.spec.ts
@@ -1,16 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, getTestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { AuthService } from 'src/commons/services/auth.service';
+
+class AuthServiceMock {
+  setAdmin() { return null; }
+  setUser() { return null; }
+}
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let service: AuthService;
+  let injector: TestBed;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [HeaderComponent],
+      providers: [
+        { provide: AuthService, useClass: AuthServiceMock }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +32,25 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set service to admin', () => {
+    injector = getTestBed();
+    service = injector.get(AuthService);
+    spyOn(service, 'setAdmin');
+
+    component.setAdmin();
+
+    expect(service.setAdmin).toHaveBeenCalledTimes(1);
+  });
+
+  it('should set service to user', () => {
+    injector = getTestBed();
+    service = injector.get(AuthService);
+    spyOn(service, 'setUser');
+
+    component.setUser();
+
+    expect(service.setUser).toHaveBeenCalledTimes(1);
   });
 });
