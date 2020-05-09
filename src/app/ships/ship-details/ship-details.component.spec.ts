117b2920a3767a82e90943a2a274a5e4df6e6634
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-05-09 17:24:00 +0200

    ship-details component tests

--- src/app/ships/ship-details/ship-details.component.spec.ts
+++ src/app/ships/ship-details/ship-details.component.spec.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
 
 describe('ShipDetailsComponent', () => {
   let component: ShipDetailsComponent;
@@ -13,10 +14,21 @@ describe('ShipDetailsComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [FormsModule, NgbAccordionModule, HttpClientTestingModule,
-        RouterTestingModule.withRoutes([]) ],
-      declarations: [ ShipDetailsComponent ]
+        RouterTestingModule.withRoutes([])],
+      declarations: [ShipDetailsComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: {
+                shipId: 5
+              }
+            }
+          }
+        }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -27,5 +39,50 @@ describe('ShipDetailsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('addDays', () => {
+    it('should add days to date', () => {
+      const date = new Date(2020, 9, 30);
+
+      const result = component.addDays(date, 5);
+
+      expect(result.getDate()).toEqual(4);
+      expect(result.getMonth()).toEqual(10);
+    });
+
+    it('should not change parameter', () => {
+      const date = new Date(2020, 10, 10);
+
+      component.addDays(date, 5);
+
+      expect(date.getDate()).toEqual(10);
+    });
+  });
+
+  describe('previousWeek', () => {
+
+    beforeEach(() => {
+      spyOn(component, 'filterSchedules');
+    });
+
+    it('should substract 7 days from weekStart and weekEnd', () => {
+      component.weekStart = new Date(2020, 5, 1);
+      component.weekEnd = new Date(2020, 9, 30);
+
+      component.previousWeek();
+
+      expect(component.weekStart.getDate()).toEqual(25);
+      expect(component.weekStart.getMonth()).toEqual(4);
+      expect(component.weekEnd.getDate()).toEqual(23);
+      expect(component.weekEnd.getMonth()).toEqual(9);
+    });
+
+    it('should filter schedules', () => {
+
+      component.previousWeek();
+
+      expect(component.filterSchedules).toHaveBeenCalledTimes(1);
+    });
   });
 });
