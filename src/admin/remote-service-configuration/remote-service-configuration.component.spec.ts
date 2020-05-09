9000436cd1dfdd99d5c31388c9b626d17b1abbc2
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-05-09 16:41:15 +0200

    remote-service-configuration component tests

--- src/admin/remote-service-configuration/remote-service-configuration.component.spec.ts
+++ src/admin/remote-service-configuration/remote-service-configuration.component.spec.ts
@@ -4,12 +4,13 @@ import { RemoteServiceConfigurationComponent } from './remote-service-configurat
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RemoteServiceConfigurationService } from '../services/remote-service-configuration.service';
-import { of } from 'rxjs';
+import { of, Observable, throwError } from 'rxjs';
 
 
 
 class RemoteServiceConfigurationServiceMock {
   getRemoteServiceConfigurationAddress() { return null; }
+  setRemoteServiceConfigurationAddress() { return null; }
 }
 
 
@@ -35,8 +36,8 @@ describe('RemoteServiceConfigurationComponent', () => {
     injector = getTestBed();
     service = injector.get(RemoteServiceConfigurationService);
     spyOn(service, 'getRemoteServiceConfigurationAddress')
-    .and
-    .returnValue(of('test'));
+      .and
+      .returnValue(of('test'));
 
     fixture.detectChanges();
     component = fixture.componentInstance;
@@ -44,5 +45,74 @@ describe('RemoteServiceConfigurationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('changeAddress successful', () => {
+    beforeEach(() => {
+      injector = getTestBed();
+      service = injector.get(RemoteServiceConfigurationService);
+      spyOn(service, 'setRemoteServiceConfigurationAddress')
+        .and
+        .returnValue(of('test'));
+
+    });
+
+    it('should change service address to new value', () => {
+      const newAddress = 'newServiceAddress';
+      component.newAddress = newAddress;
+      component.changeAddress();
+
+      expect(service.setRemoteServiceConfigurationAddress)
+        .toHaveBeenCalledWith(component.newAddress);
+    });
+
+    it('should stop loading and edit', () => {
+      const newAddress = 'newServiceAddress';
+      component.newAddress = newAddress;
+      component.changeAddress();
+
+      expect(component.isLoading).toEqual(false);
+      expect(component.isEdit).toEqual(false);
+    });
+  });
+
+  describe('changeAddress error', () => {
+    beforeEach(() => {
+      injector = getTestBed();
+      service = injector.get(RemoteServiceConfigurationService);
+      spyOn(service, 'setRemoteServiceConfigurationAddress')
+        .and
+        .returnValue(throwError({ status: 400 }));
+    });
+
+    it('should not change service address to new value', () => {
+      const newAddress = 'newServiceAddress';
+      const oldAddress = 'oldServiceAddress';
+      component.newAddress = newAddress;
+      component.serviceAddress = oldAddress;
+      component.changeAddress();
+
+      expect(component.serviceAddress).toEqual(oldAddress);
+    });
+  });
+
+  describe('switchToEdit', () => {
+    it('should set isEdit to true', () => {
+      component.isEdit = false;
+
+      component.switchToEdit();
+
+      expect(component.isEdit).toEqual(true);
+    });
+  });
+
+  describe('switchToView', () => {
+    it('should set isEdit to false', () => {
+      component.isEdit = true;
+
+      component.switchToView();
+
+      expect(component.isEdit).toEqual(false);
+    });
   });
 });
