148f0b6e40512d506eed471fff03cf6326192a82
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-05-09 16:58:24 +0200

    schedule.service tests

--- src/app/services/schedules/schedule.service.spec.ts
+++ src/app/services/schedules/schedule.service.spec.ts
@@ -1,15 +1,140 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 
 import { ScheduleService } from './schedule.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Schedule } from 'src/commons/entities/Ship';
 
 describe('ScheduleService', () => {
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
+  const apiUrl = 'api/Schedule/';
   beforeEach(() => TestBed.configureTestingModule({
     imports: [HttpClientTestingModule]
   }));
 
+  beforeEach(() => {
+    injector = getTestBed();
+    httpMock = injector.get(HttpTestingController);
+  });
+
   it('should be created', () => {
     const service: ScheduleService = TestBed.get(ScheduleService);
     expect(service).toBeTruthy();
+  });
+
+  describe('deleteSchedule', () => {
+    it('should send delete request with id in url', () => {
+      const idToDelete = 5;
+      const url = `${apiUrl}${idToDelete}/`;
+      const service: ScheduleService = TestBed.get(ScheduleService);
+      service.deleteSchedule(idToDelete)
+        .subscribe(x => {
+          expect(x).toEqual({});
+        });
+
+      const testRequest = httpMock.expectOne(url);
+      expect(testRequest.request.method).toBe('DELETE');
+      testRequest.flush({});
+
+      httpMock.verify();
+
+    });
+  });
+
+  describe('addSchedule', () => {
+    it('should send post request with schedule and id in url', () => {
+      const shipId = 5;
+      const url = `${apiUrl}${shipId}/`;
+      const scheduleToAdd: Schedule = {
+        id: 0,
+        arrival: new Date(),
+        departure: new Date()
+      };
+      const service: ScheduleService = TestBed.get(ScheduleService);
+      service.addSchedule(scheduleToAdd, shipId)
+        .subscribe(x => {
+          expect(x).toEqual({});
+        });
+
+      const testRequest = httpMock.expectOne(url);
+      expect(testRequest.request.body).toBe(scheduleToAdd);
+      expect(testRequest.request.method).toBe('POST');
+      testRequest.flush({});
+
+      httpMock.verify();
+
+    });
+
+    describe('addSchedule', () => {
+      it('should send post request with schedule and shipId in url', () => {
+        const shipId = 5;
+        const url = `${apiUrl}${shipId}/`;
+        const scheduleToAdd: Schedule = {
+          id: 0,
+          arrival: new Date(),
+          departure: new Date()
+        };
+        const service: ScheduleService = TestBed.get(ScheduleService);
+        service.addSchedule(scheduleToAdd, shipId)
+          .subscribe(x => {
+            expect(x).toEqual({});
+          });
+
+        const testRequest = httpMock.expectOne(url);
+        expect(testRequest.request.body).toBe(scheduleToAdd);
+        expect(testRequest.request.method).toBe('POST');
+        testRequest.flush({});
+
+        httpMock.verify();
+
+      });
+    });
+    describe('editSchedule', () => {
+      it('should send put request with schedule', () => {
+        const url = `${apiUrl}`;
+        const scheduleToEdit: Schedule = {
+          id: 0,
+          arrival: new Date(),
+          departure: new Date()
+        };
+        const service: ScheduleService = TestBed.get(ScheduleService);
+        service.editSchedule(scheduleToEdit)
+          .subscribe(x => {
+            expect(x).toEqual({});
+          });
+
+        const testRequest = httpMock.expectOne(url);
+        expect(testRequest.request.body).toBe(scheduleToEdit);
+        expect(testRequest.request.method).toBe('PUT');
+        testRequest.flush({});
+
+        httpMock.verify();
+
+      });
+    });
+
+    describe('bulkAdd', () => {
+      it('should send put request with schedules and shipId in url', () => {
+        const shipId = 6;
+        const url = `${apiUrl}BulkAdd/${shipId}/`;
+        const schedulesToAdd: Schedule[] = [{
+          id: 0,
+          arrival: new Date(),
+          departure: new Date()
+        }];
+        const service: ScheduleService = TestBed.get(ScheduleService);
+        service.bulkAdd(schedulesToAdd, shipId)
+          .subscribe(x => {
+            expect(x).toEqual({});
+          });
+
+        const testRequest = httpMock.expectOne(url);
+        expect(testRequest.request.body).toBe(schedulesToAdd);
+        expect(testRequest.request.method).toBe('POST');
+        testRequest.flush({});
+
+        httpMock.verify();
+      });
+    });
   });
 });
--- src/app/services/schedules/schedule.service.ts
+++ src/app/services/schedules/schedule.service.ts
@@ -11,15 +11,15 @@ export class ScheduleService {
   constructor(private http: HttpClient) { }
 
   deleteSchedule(scheduleId: number) {
-    return this.http.delete(`${this.apiUrl}/${scheduleId}`);
+    return this.http.delete(`${this.apiUrl}${scheduleId}/`);
   }
   addSchedule(schedule: Schedule, shipId: number) {
-    return this.http.post(`${this.apiUrl}${shipId}`, schedule);
+    return this.http.post(`${this.apiUrl}${shipId}/`, schedule);
   }
   editSchedule(schedule: Schedule) {
     return this.http.put(`${this.apiUrl}`, schedule);
   }
   bulkAdd(schedules: Schedule[], shipId: number) {
-    return this.http.post(`${this.apiUrl}/BulkAdd/${shipId}`, schedules);
+    return this.http.post(`${this.apiUrl}BulkAdd/${shipId}/`, schedules);
   }
 }
